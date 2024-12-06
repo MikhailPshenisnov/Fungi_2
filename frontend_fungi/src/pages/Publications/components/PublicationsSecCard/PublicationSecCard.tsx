2268475dab65ac866e236f6ad144100c52c17163
smozhaev <kmozhaev100@gmail.com>
2024-12-06 14:23:58 +0300

    Создал переисользуемую основу для создания карточек и внедрил ее

--- frontend_fungi/src/pages/Publications/components/PublicationsSecCard/PublicationSecCard.tsx
+++ frontend_fungi/src/pages/Publications/components/PublicationsSecCard/PublicationSecCard.tsx
@@ -1,23 +1,26 @@
 import React from "react";
 import "./index.css";
-import { TPublicationsCard} from "../../types.ts";
+import { TPublicationsCard } from "../../types";
+import { Card } from "../../../../components/shared/ui/base/Card/Card";
 
 interface IPublicationsCard {
-    card: TPublicationsCard
-};
+    card: TPublicationsCard;
+    onClick?: () => void;
+}
 
-const PublicationsSecCard: React.FC<IPublicationsCard> = ({card}) => {
-
+const PublicationsSecCard: React.FC<IPublicationsCard> = ({ card, onClick }) => {
     return (
-            <div className="publication-sec-card">
-                <img className="publication-sec-card__image" src="/images/png/alt-card-image.png" alt="image1" />
-                <div className="publication-info">
-                    <h3 className="publications-sec-card__title">{card.title}</h3>
-                    <p>читать продолжение в источнике...</p>
-                </div>
+        <Card className="publication-sec-card" onClick={onClick}>
+            <img 
+                className="publication-sec-card__image" 
+                src="/images/png/alt-card-image.png" 
+                alt="image1" 
+            />
+            <div className="publication-info">
+                <h3 className="publications-sec-card__title">{card.title}</h3>
+                <p>читать продолжение в источнике...</p>
             </div>
-
-            
+        </Card>
     );
 }
 
