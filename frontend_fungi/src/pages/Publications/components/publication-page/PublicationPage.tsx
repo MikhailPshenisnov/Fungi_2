3ce4122ace756847bee1f82cd532379b5d359a62
Sonya Nadtochey <nadtocheysonya@gmail.com>
2025-04-17 22:26:00 +0300

    изменение страницы публикации

--- frontend_fungi/src/pages/Publications/components/publication-page/PublicationPage.tsx
+++ frontend_fungi/src/pages/Publications/components/publication-page/PublicationPage.tsx
@@ -2,6 +2,9 @@ import './PublicationPage.css';
 import { usePublication } from '../../model/hooks';
 import React, { useState } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
+import { PublicationsCard } from '../publications-card';
+import { mockPublications } from '@shared/const/mock/publications';
+
 
 export const PublicationPage: React.FC = () => {
     const { id } = useParams();
@@ -12,6 +15,8 @@ export const PublicationPage: React.FC = () => {
     if (!publication) {
         return <div>Публикация не найдена</div>;
     }
+
+    const recommendedPublications = mockPublications.slice(0, 2);
 
     const handleBack = () => {
         navigate(-1);
@@ -24,55 +29,72 @@ export const PublicationPage: React.FC = () => {
     return (
         <div className="publication-page">
             <div className="publication-page__content">
-                <button
-                    onClick={handleBack}
-                    className="publication-page__back-button"
-                >
-                    <img src="/images/svg/arrow.svg" alt="Назад" />
-                    <span>Назад</span>
-                </button>
-                <div className="publication-page_container">
-                    <h1 className="publication-page__title">
-                        {publication.title}
-                    </h1>
-                    <div className="publication-page__info">
+                <div className='publication-page__info-container'>
+                    <div className="publication-page_container">
+                        <button
+                            onClick={handleBack}
+                            className="publication-page__back-button"
+                        >
+                            <img src="/images/svg/arrow.svg" alt="Назад" />
+                            <span>Назад</span>
+                        </button>
+                    
+                        <h1 className="publication-page__title">
+                            {publication.title}
+                        </h1>
+                        
                         <p className="publication-page__author">
                             Автор: {publication.author}
                         </p>
-                        <p className="publication-page__date">
-                            {new Date(
-                                publication.publishDate
-                            ).toLocaleDateString('ru-RU', {
-                                year: 'numeric',
-                                month: 'long',
-                                day: 'numeric',
-                            })}
-                        </p>
                     </div>
+                    <div className='publication-page__like-button'>
+                        <button className='publication-page__like-button-img'>
+                            <img src="/images/svg/bookmark.svg" alt="Сохранить" />
+                        </button>
+                        
+                    </div>
+                    
+                    
                 </div>
                 <img
-                    className="publication-page__image"
-                    src={
-                        imageError
-                            ? '/images/png/alt-card-image.png'
-                            : publication.headerPhotoLink
-                    }
-                    alt={publication.title}
-                    onError={handleImageError}
-                />
+                        className="publication-page__image"
+                        src={
+                            imageError
+                                ? '/images/png/alt-card-image.png'
+                                : publication.headerPhotoLink
+                        }
+                        alt={publication.title}
+                        onError={handleImageError}
+                    />
 
-                <div className="publication-page__text">
-                    {publication.paragraphs.map(
-                        (paragraph, index) =>
-                            paragraph && (
-                                <p
-                                    key={index}
-                                    className="publication-page__paragraph"
-                                >
-                                    {paragraph}
-                                </p>
-                            )
-                    )}
+                <div className='publication-page__main-info'>
+                    <div className='publication-page__main-text'>
+                        <div className="publication-page__text">
+                            {publication.paragraphs.map(
+                                (paragraph, index) =>
+                                    paragraph && (
+                                        <p
+                                            key={index}
+                                            className="publication-page__paragraph"
+                                        >
+                                            {paragraph}
+                                        </p>
+                                    )
+                            )}
+                        </div>
+                    </div>
+                    <div className='publication-text__recommendation'>
+                        <h3>Рекомендуемые статьи</h3>
+                        <div className="publication-text__recommendations-container">
+                        {recommendedPublications.map((publication) => (
+                            <PublicationsCard
+                            key={publication.id}
+                            card={publication}
+                            onClick={() => navigate(`/publications/${publication.id}`)}
+                            />
+                        ))}
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
